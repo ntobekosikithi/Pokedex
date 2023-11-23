1a46aabe9bf9bae1efc378b908340a3f6f2b7a5c
Ntobeko Sikithi <ntobekosikithi@woolworths.co.za>
2023-11-23 07:08:42 +0200

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by ns90840 on 2023/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
